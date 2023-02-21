d4ad1ec408bc4faf58db9d2db0e2b1fab47847bf
Edie FanYidi <71816893+FanYidi@users.noreply.github.com>
2023-02-21 09:50:15 +0800

    PAT_B1001

---
+++ chapter 3/PAT_B1001.cpp
@@ -0,0 +1,16 @@
+/* Callatz: (n*3 + 1)/2 -->1 */
+
+#include <cstdio>
+
+int main(){
+    int n, step = 0;
+    printf("请输入正整数n:");
+    scanf("%d", &n);
+    while(n != 1){
+        if(n%2 == 0) n /= 2;
+        else n = (n*3 + 1)/2;
+        step += 1;
+    }
+    printf("最终需要 %d 步来变为1。",step);
+    return 0;
+}
