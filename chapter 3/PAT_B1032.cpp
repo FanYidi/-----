d6c7afda23f957278cb48f9a5342dc4090871f06
Edie FanYidi <71816893+FanYidi@users.noreply.github.com>
2023-02-23 10:36:58 +0800

    PAT_B1032

---
+++ chapter 3/PAT_B1032.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+
+int main(){
+    const int maxn = 100000;
+    int n;
+    scanf("%d", &n);
+    int lis[maxn] = {0};
+    int maxnum=0, maxscore=0;
+    for(int i = 0; i < n; i++){
+        int num, score;
+        scanf("%d %d", &num, &score); //可以不加空格
+        lis[num] += score;
+        //printf("得到了数组内容：%d %d", num, lis[num]); //测试
+        if(maxscore < lis[num]){
+            maxscore = lis[num];
+            maxnum = num;
+        }
+    }
+    printf("%d %d", maxnum,lis[maxnum]);
+    return 0;
+}
